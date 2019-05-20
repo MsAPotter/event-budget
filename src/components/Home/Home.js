459d4bda142c5904d634dfb9dd303d420e8bebeb
Atasha Potter <Ms.APotter@gmail.com>
2019-05-20 10:30:54 -0400

    nav component mvp

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
 import './Home.css'
 import {Link} from 'react-router-dom';
-// import SideNav, {MenuIcon} from 'react-simple-sidenav';
-// import { Sidenav, Toggle, Nav, Icon } from 'rsuite';
 
 class Home extends Component {
     constructor(props) {
         super()
-
-   
     }
-
     
     render() {
         return (
             <div className="sidebar">
-            <Link to="#home"><i className="sidebar-nav fa fa-fw fa-home"></i> Home</Link>
-            <Link to="#services"><i className="sidebar-nav fas fa-file-invoice-dollar"></i> Expenses</Link>
-            <Link to="#clients"><i className="sidebar-nav fas fa-money-bill"></i> Income</Link>
-            <Link to="#contact"><i className="sidebar-nav fas fa-calendar-check"></i> Event</Link>
-                
+            <Link to="#home"><i className="sidebar-nav fa fa-home"></i> Home</Link>
+            <Link to="#services"><i className="sidebar-nav fa fa-credit-card-alt"></i> Expenses</Link>
+            <Link to="#clients"><i className="sidebar-nav fa fa-money"></i> Income</Link>
+            {/* <Link to="#contact"><i className="sidebar-nav fa fa-fw fa-calendar-check-o"></i> Event</Link> */}
+            {/* <Link to="#contact"><i className="sidebar-nav fa fa-car"></i> Event</Link>     */}
+            <Link to="#contact"><i className="sidebar-nav fa fa-smile-o"></i> Event</Link> 
             </div>
         );
     }
