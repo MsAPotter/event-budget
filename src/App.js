cd20ad05cac05f2079a967eaa0b589a1d7bfba95
Atasha Potter <Ms.APotter@gmail.com>
2019-05-21 06:54:02 -0400

    misc changes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Home from './components/Home/Home';
 import EventCost from './components/EventCost/EventCost';
+import Expenses from './components/Expenses/Expenses';
 
 const newEvents = [];
+const TotalCosts = [];
 
 class App extends Component {
   constructor(props) {
     super()
 
-    this.state = {newEvents: newEvents}
+    this.state = {
+      newEvents: newEvents,
+      TotalCosts: TotalCosts
+    }
 
-    this.addEvent = this.addEvent.bind(this)
+    this.addEvent = this.addEvent.bind(this);
+    this.addTotalCost = this.addTotalCost.bind(this);
   }
 
   addEvent(newEvent) {
@@ -19,6 +25,13 @@ class App extends Component {
     let newEvents = this.state.newEvents.concat([newEvent])
     console.log(newEvents)
     this.setState({newEvents: newEvents})
+  }
+
+  addTotalCost(totalCost) {
+    console.log(totalCost)
+    let TotalCosts = this.state.TotalCosts.concat([totalCost])
+    console.log(TotalCosts)
+    this.setState({TotalCosts: TotalCosts})
   }
 
 
@@ -42,6 +55,15 @@ class App extends Component {
             exact path="/eventcost"
             render={routerProps => (
               <EventCost 
+              addTotalCost={this.addTotalCost}
+              {...this.state} />
+            )}
+          />
+
+        <Route
+            exact path="/expenses"
+            render={routerProps => (
+              <Expenses
               {...routerProps} 
               {...this.state} />
             )}
--- src/components/EventCost/EventCost.js
+++ src/components/EventCost/EventCost.js
@@ -1,12 +1,153 @@
 import React, { Component } from 'react';
+import '../EventCost/EventCost.css';
+import Expenses from '../Expenses/Expenses';
 
 class EventCost extends Component {
+    constructor(props) {
+        super()
+
+        this.state = {
+            planeticket: 0,
+            gas: 0,
+            accomodation: 0,
+            transportation: 0,
+            food: 0,
+            total: 0
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        // this.handleTotal = this.handleTotal.bind(this);
+        this.handleTotalCost = this.handleTotalCost.bind(this);
+    }
+    
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value,
+            total: event.target.value
+        })
+    }
+
+    // handleTotal(event) {
+    //     event.preventDefault();
+    //     let total = 0;
+    //     console.log(this.state.length)
+    //     for(var i=0;i<6;i++){
+    //         if(parseInt(this.state[i].value))
+    //             total += parseInt(this.state[i].value);
+    //     }
+    // }
+
+    handleTotalCost(event) {
+        event.preventDefault();
+        let totalCost = {
+            total: this.state.total
+        }
+        console.log(totalCost)
+        this.props.addTotalCost(totalCost)
+
+        this.setState({'submitted': true })
+    }
+
+
     render() {
+        if (this.state.submitted) {
+            return <Expenses />
+        } else {
         return (
-            <div>
-                <h1>How Much Will It Cost?</h1>
+            <div className="container2">
+            <div className="prompt2">
+                <h1>How Much Will It Cost? (Round up!)</h1>
+
+                <form 
+                className="form"
+                onSubmit={this.handleTotalCost}>
+
+                    <div className="small-box">
+                    <p>Plane ticket cost</p>
+                    <input
+                    className="event-box-prices" 
+                    type="text" 
+                    id="planeticket"
+                    name="planeticket"
+                    value={this.props.planeticket}
+                    // placeholder="1500"
+                    onChange={this.handleplaneticket}
+                    
+                    ></input>
+                    </div>
+
+                    <div className="small-box">
+                    <p>Gas cost</p>
+                    <input 
+                    className="event-box-prices"
+                    type="text" 
+                    id="gas"
+                    value={this.props.gas}
+                    placeholder="100"
+                    onChange={this.handleGas}
+                    name="gas"
+                    ></input>
+                    </div>
+
+                    <div className="small-box">
+                    <p>Hotel/ Airbnb</p>
+                    <input 
+                    className="event-box-prices"
+                    type="text" 
+                    id="accomodation"
+                    value={this.props.gas}
+                    placeholder="1000"
+                    onChange={this.handleChange}
+                    name="accomodation"
+                    />
+                    </div>
+
+                    <div className="small-box">
+                    <p>Train/ Uber costs</p>
+                    <input 
+                    className="event-box-prices"
+                    type="text" 
+                    id="transportation"
+                    value={this.props.gas}
+                    placeholder="100"
+                    onChange={this.handleChange}
+                    name="transportation"
+                    />
+                    </div>
+
+                    <div className="small-box">
+                    <p>Food estimate</p>
+                    <input
+                    className="event-box-prices" 
+                    type="text" 
+                    id="food"
+                    value={this.props.gas}
+                    placeholder="300"
+                    onChange={this.handleChange}
+                    name="food"
+                    />
+                    </div>
+
+                    <div className="small-box">
+                    <p>TOTAL</p>
+                    <input
+                    className="event-box-prices" 
+                    type="text" 
+                    id="total"
+                    name="total"
+                    value={this.state.total}
+                    onChange={this.handleTotal}
+                    >{this.total}</input>
+                    </div>
+
+                    <button type="submit">Submit</button>
+
+                </form>
+                </div>
+
             </div>
         );
+        }
     }
 }
 
