8fd6efeaf373d9331bbfc2e511e41727d376adb6
Adam Grider <adam.h.grider@gmail.com>
2019-05-22 10:59:24 -0400

    fix nav and stuff

--- src/App.js
+++ src/App.js
@@ -1,101 +1,73 @@
 import React, { Component } from 'react';
-import Sidenav from './components/Sidenav/Sidenav'
-import {Route, Switch} from 'react-router-dom';
+import Sidenav from './components/Sidenav/Sidenav';
+import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Expenses from './components/Expenses/Expenses';
 import EventCost from './components/EventCost/EventCost';
+import './App.css';
 
 const newEvents = [];
 const TotalCosts = [];
 
 class App extends Component {
-  constructor(props) {
-    super()
+	constructor(props) {
+		super(props);
+		this.state = {
+			newEvents: newEvents,
+			TotalCosts: TotalCosts
+		};
+		this.addEvent = this.addEvent.bind(this);
+		this.addTotalCost = this.addTotalCost.bind(this);
+	}
 
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <Switch>
-//           <Route
-//             exact path="/"
-//             render={routerProps => (
-//               <Sidenav 
-//               {...routerProps} 
+	addEvent(newEvent) {
+		console.log('App: addEvent');
+		console.log(newEvent);
+		let newEvents = this.state.newEvents.concat([ newEvent ]);
+		console.log(newEvents);
+		this.setState({ newEvents: newEvents });
+	}
+	addTotalCost(totalCost) {
+		console.log('App: addTotalCost');
+		console.log(totalCost);
+		let TotalCosts = this.state.TotalCosts.concat([ totalCost ]);
+		console.log(TotalCosts);
+		this.setState({ TotalCosts: TotalCosts });
+	}
 
-    this.state = {
-      newEvents: newEvents,
-      TotalCosts: TotalCosts
-    }
-
-    this.addEvent = this.addEvent.bind(this);
-    this.addTotalCost = this.addTotalCost.bind(this);
-
-  }
-
-  addEvent(newEvent) {
-    console.log(newEvent)
-    let newEvents = this.state.newEvents.concat([newEvent])
-    console.log(newEvents)
-    this.setState({newEvents: newEvents})
-  }
-
-
-  addTotalCost(totalCost) {
-    console.log(totalCost)
-    let TotalCosts = this.state.TotalCosts.concat([totalCost])
-    console.log(TotalCosts)
-    this.setState({TotalCosts: TotalCosts})
-  }
-
-
-  render() {
-    console.log('App: render')
-    console.log(this.state.newEvents)
-    console.log(this.state.TotalCosts)
-
-    return (
-      <div>
-        <Switch>
-   
-        <Route
-          exact path="/"
-          render={routerProps => (
-            <Sidenav 
-            {...routerProps} />
-      )}
-  />
-      
-        <Route
-            exact path="/"
-            render={routerProps => (
-              <Home 
-              addEvent={this.addEvent}
-              {...this.state} />
-            )}
-          />
-
-            exact path="/eventcost"
-            render={routerProps => (
-              <EventCost 
-              addTotalCost={this.addTotalCost}
-              {...this.state} />
-            )}
-          />
-
-        <Route
-            exact path="/expenses"
-            render={routerProps => (
-              <Expenses
-              {...routerProps} 
-              {...this.state} />
-            )}
-          />
-        </Switch>
-        
-      </div>
-    );
-  }
+	render() {
+		console.log('App: render');
+		console.log(this.state.newEvents);
+		console.log(this.state.TotalCosts);
+		return (
+			<div className="App">
+				<div className="App-Nav">
+					<Sidenav />
+				</div>
+				<div className="App-Main">
+					<Switch>
+						<Route
+							exact
+							path="/"
+							render={(routerProps) => <Home addEvent={this.addEvent} {...this.state} {...routerProps} />}
+						/>
+						<Route
+							exact
+							path="/eventcost"
+							render={(routerProps) => (
+								<EventCost addTotalCost={this.addTotalCost} {...this.state} {...routerProps} />
+							)}
+						/>
+						<Route
+							exact
+							path="/expenses"
+							render={(routerProps) => <Expenses {...routerProps} {...this.state} />}
+						/>
+					</Switch>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,103 +1,99 @@
 import React, { Component } from 'react';
 import '../Home/Home.css';
 import EventCost from '../EventCost/EventCost';
-import {Link} from 'react-router-dom';
 
 let placeholder = [
-    `Take me to Greece!`,
-    "Road trip to Mount Rushmore",
-    "Honeymoon in Hawaii",
-    "Bermuda, Bahama, come on pretty mama"
-]
+	'Take me to Greece!',
+	'Road trip to Mount Rushmore',
+	'Honeymoon in Hawaii',
+	'Bermuda, Bahama, come on pretty mama'
+];
 
 class Home extends Component {
-    constructor(props) {
-        super()
+	constructor(props) {
+		super(props);
+		this.state = {
+			input: '',
+			trip: ''
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.eventDetails = this.eventDetails.bind(this);
+		this.handleEventDetails = this.handleEventDetails.bind(this);
+	}
 
-        this.state = {
-            input: '',
-            trip: ''
-          }
+	componentDidMount() {
+		console.log('Home: componentDidMount');
+		var x = Math.floor(Math.random() * placeholder.length);
+		placeholder = placeholder[x];
+		console.log(placeholder);
+		this.setState({
+			placeholder: placeholder
+		});
+	}
 
-        this.handleChange = this.handleChange.bind(this);
-        this.eventDetails = this.eventDetails.bind(this);
-      this.handleEventDetails = this.handleEventDetails.bind(this);
-    }
+	handleChange(event) {
+		console.log('Home: handleChange');
+		this.setState({
+			input: event.target.value
+		});
+	}
 
-    componentDidMount() {
-        var x = Math.floor(Math.random() * placeholder.length);
-        placeholder = placeholder[x]
-        console.log(placeholder)
-        this.setState({
-            placeholder: placeholder
-          })
-    }
+	eventDetails(trip) {
+		console.log('Home: eventDetails');
+		let newEvent = {
+			trip: this.state.trip
+		};
+		this.props.addEvent(newEvent);
+	}
 
-    handleChange(event) {
-        this.setState({
-          input: event.target.value
-        })
-      }
+	handleEventDetails(event) {
+		console.log('Home: handleEventDetails');
+		event.preventDefault();
+		let newEvent = {
+			trip: this.state.input
+		};
+		console.log(newEvent);
+		this.props.addEvent(newEvent);
 
-    eventDetails(trip) {
-        let newEvent = {
-            trip: this.state.trip
-        }
-        this.props.addEvent(newEvent)
-        }
-  
-//     render() {
+		this.setState({ submitted: true });
+		// Reference:  https://stackoverflow.com/questions/28907965/how-do-i-add-a-component-after-an-submit-event-using-reactjs
+	}
 
-    handleEventDetails(event) {
-        event.preventDefault();
-        let newEvent = {
-            trip: this.state.input
-        }
-        console.log(newEvent)
-        this.props.addEvent(newEvent)
+	render() {
+		console.log('Home: render');
+		// console.log(placeholder)
+		if (this.state.submitted) {
+			return <EventCost />;
+		} else {
+			return (
+				<div className="container">
+					<div className="content">
+						<h1>Where do you want to go?</h1>
+						<h1>What do you want to do?</h1>
 
-        this.setState({'submitted': true })
-// Reference:  https://stackoverflow.com/questions/28907965/how-do-i-add-a-component-after-an-submit-event-using-reactjs
-        
-}
-  
-    render() {
-        // console.log(placeholder)
-        if (this.state.submitted) {
-            return <EventCost />
-        } else {
-        return (
-            <div className="container">
-                <div className="content">
-                    <h1>Where do you want to go?</h1>
-                    <h1>What do you want to do?</h1>
+						{/* <input
+							type="text"
+							name="event"
+							id="event"
+							placeholder="Take me to Greece!"
+							onChange={this.handleChange}
+							onSubmit={this.eventDetails}
+						/> */}
 
-                    <input 
-                    type="text" 
-                    name="event"
-                    id="event"
-                    placeholder="Take me to Greece!"
-                    onChange={this.handleChange}
-                    onSubmit={this.eventDetails}
-                    />
-
-                    <form onSubmit={this.handleEventDetails}>
-                    <input 
-                    type="text" 
-                    id="trip"
-                    value={this.props.trip}
-                    placeholder= {placeholder}
-                    onChange={this.handleChange}
-                    />
-                    </form>
-
-                </div>
-            </div>
-        );
-    }
-
-    }
-
+						<form onSubmit={this.handleEventDetails}>
+							<input
+								type="text"
+								id="trip"
+								value={this.props.trip}
+								placeholder={placeholder}
+								onChange={this.handleChange}
+							/>
+						</form>
+					</div>
+				</div>
+			);
+		}
+	}
 }
 
 export default Home;
