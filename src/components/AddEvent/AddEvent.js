007b7b122d7b5ec0c3c09ec037c294fb584df553
Adam Grider <adam.h.grider@gmail.com>
2019-05-22 13:09:25 -0400

    add event logic

--- src/components/AddEvent/AddEvent.js
+++ src/components/AddEvent/AddEvent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 import './AddEvent.css';
 
@@ -15,8 +16,10 @@ class AddEvent extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			input: '',
-			trip: ''
+			trip: '',
+			startDate: '',
+			endDate: '',
+			submitted: false
 		};
 		this.handleChange = this.handleChange.bind(this);
 		// this.eventDetails = this.eventDetails.bind(this);
@@ -26,7 +29,7 @@ class AddEvent extends Component {
 	handleChange(event) {
 		console.log('Home: handleChange');
 		this.setState({
-			input: event.target.value
+			[event.target.name]: event.target.value
 		});
 	}
 
@@ -42,15 +45,17 @@ class AddEvent extends Component {
 		console.log('Home: handleEventDetails');
 		event.preventDefault();
 		let newEvent = {
-			name: this.state.input
+			name: this.state.name,
+			dates: {
+				start: this.state.startDate,
+				end: this.state.endDate
+			}
 		};
 		console.log(newEvent);
 		axios.post(`https://event-budget-api.herokuapp.com/api/${userId}/events`, newEvent).then((event) => {
 			console.log(event);
 		});
-		// this.props.addEvent(newEvent);
-
-		// this.setState({ submitted: true });
+		this.setState({ submitted: true });
 		// Reference:  https://stackoverflow.com/questions/28907965/how-do-i-add-a-component-after-an-submit-event-using-reactjs
 	}
 
@@ -65,12 +70,15 @@ class AddEvent extends Component {
 	}
 
 	render() {
-		return (
-			<div className="content">
-				<h1>Where do you want to go?</h1>
-				<h1>What do you want to do?</h1>
+		if (this.state.submitted === true) {
+			return <Redirect to="/event-cost" />;
+		} else {
+			return (
+				<div className="content">
+					<h1>Where do you want to go?</h1>
+					<h1>What do you want to do?</h1>
 
-				{/* <input
+					{/* <input
 							type="text"
 							name="event"
 							id="event"
@@ -79,17 +87,22 @@ class AddEvent extends Component {
 							onSubmit={this.eventDetails}
 						/> */}
 
-				<form onSubmit={this.handleEventDetails}>
-					<input
-						type="text"
-						id="trip"
-						// value={this.state.trip}
-						placeholder={this.state.placeholder}
-						onChange={this.handleChange}
-					/>
-				</form>
-			</div>
-		);
+					<form onSubmit={this.handleEventDetails}>
+						<input
+							type="text"
+							name="name"
+							// value={this.state.trip}
+							placeholder={this.state.placeholder}
+							onChange={this.handleChange}
+						/>
+						<div>
+							<input type="date" name="startDate" onChange={this.handleChange} />
+							<input type="date" name="endDate" onChange={this.handleChange} />
+						</div>
+					</form>
+				</div>
+			);
+		}
 	}
 }
 
